90483bab34c08ad463d0c0a688e86c0cd033d3de
luricisme <trinhnguyenluong182306@gmail.com>
2025-06-14 22:42:55 +0700

    Test log grafana

--- spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/VisitsServiceApplication.java
+++ spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/VisitsServiceApplication.java
@@ -26,6 +26,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class VisitsServiceApplication {
     // I test for Jenkins #4 test ets
+    // Toi test nay
     public static void main(String[] args) {
         SpringApplication.run(VisitsServiceApplication.class, args);
     }
